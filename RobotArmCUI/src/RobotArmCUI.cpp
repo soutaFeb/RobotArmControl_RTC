6142aab0d9954161381ec166019904b5a63a4812
soutaFeb <72483942+soutaFeb@users.noreply.github.com>
2020-10-18 10:43:13 +0900

    first commit

---
+++ RobotArmCUI/src/RobotArmCUI.cpp
@@ -0,0 +1,225 @@
+﻿// -*- C++ -*-
+/*!
+ * @file  RobotArmCUI.cpp
+ * @brief ModuleDescription
+ * @date $Date$
+ *
+ * $Id$
+ */
+
+#include "RobotArmCUI.h"
+
+// Module specification
+// <rtc-template block="module_spec">
+static const char* robotarmcui_spec[] =
+  {
+    "implementation_id", "RobotArmCUI",
+    "type_name",         "RobotArmCUI",
+    "description",       "ModuleDescription",
+    "version",           "1.0.0",
+    "vendor",            "VenderName",
+    "category",          "Category",
+    "activity_type",     "PERIODIC",
+    "kind",              "DataFlowComponent",
+    "max_instance",      "1",
+    "language",          "C++",
+    "lang_type",         "compile",
+    ""
+  };
+// </rtc-template>
+
+/*!
+ * @brief constructor
+ * @param manager Maneger Object
+ */
+RobotArmCUI::RobotArmCUI(RTC::Manager* manager)
+    // <rtc-template block="initializer">
+  : RTC::DataFlowComponentBase(manager),
+    m_targetPos_outputOut("targetPos_output", m_targetPos_output)
+
+    // </rtc-template>
+{
+}
+
+/*!
+ * @brief destructor
+ */
+RobotArmCUI::~RobotArmCUI()
+{
+}
+
+
+
+RTC::ReturnCode_t RobotArmCUI::onInitialize()
+{
+  // Registration: InPort/OutPort/Service
+  // <rtc-template block="registration">
+  // Set InPort buffers
+  
+  // Set OutPort buffer
+  addOutPort("targetPos_output", m_targetPos_outputOut);
+  
+  // Set service provider to Ports
+  
+  // Set service consumers to Ports
+  
+  // Set CORBA Service Ports
+  
+  // </rtc-template>
+
+  // <rtc-template block="bind_config">
+  // </rtc-template>
+  
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t RobotArmCUI::onFinalize()
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RobotArmCUI::onStartup(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RobotArmCUI::onShutdown(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+RTC::ReturnCode_t RobotArmCUI::onActivated(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t RobotArmCUI::onDeactivated(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t RobotArmCUI::onExecute(RTC::UniqueId ec_id)
+{
+    
+    m_targetPos_output.data.length(3);
+
+    std::cout << "" << std::endl;
+    std::cout << "Please input target coordinates: " << std::endl;
+
+    using std::string;
+    string x;
+    string y;
+    string z;
+    int signX = 1;
+    int signY = 1;
+    int signZ = 1;
+    bool xFlag = false;
+    bool yFlag = false;
+    bool zFlag = false;
+    std::cout << " X : ";
+    std::cin >> x;
+    if (x[0] == '-') {
+        x = x.replace(0, 1, "");
+        signX = -1;
+    }
+    if (std::all_of(x.cbegin(), x.cend(), isdigit) && x.size() > 0) {
+        m_targetPos_output.data[0] = signX * std::stod(x);
+        xFlag = true;
+    }
+    if (xFlag) {
+        std::cout << " Y : ";
+        std::cin >> y;
+        if (y[0] == '-') {
+            y = y.replace(0, 1, "");
+            signY = -1;
+        }
+        if (std::all_of(y.cbegin(), y.cend(), isdigit) && y.size() > 0) {
+            m_targetPos_output.data[1] = signY * std::stod(y);
+            yFlag = true;
+        }
+    }
+    if (xFlag && yFlag) {
+        std::cout << " Z : ";
+        std::cin >> z;
+        if (z[0] == '-') {
+            z = z.replace(0, 1, "");
+            signZ = -1;
+        }
+        if (std::all_of(z.cbegin(), z.cend(), isdigit) && z.size() > 0) {
+            m_targetPos_output.data[2] = signZ * std::stod(z);
+            zFlag = true;
+        }
+    }
+    if (xFlag && yFlag && zFlag) {
+        m_targetPos_outputOut.write();
+    }
+    else {
+        std::cout << "Canceled " << std::endl;
+        xFlag = false;
+        yFlag = false;
+        zFlag = false;
+    }
+
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t RobotArmCUI::onAborting(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RobotArmCUI::onError(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RobotArmCUI::onReset(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RobotArmCUI::onStateUpdate(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RobotArmCUI::onRateChanged(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+
+extern "C"
+{
+ 
+  void RobotArmCUIInit(RTC::Manager* manager)
+  {
+    coil::Properties profile(robotarmcui_spec);
+    manager->registerFactory(profile,
+                             RTC::Create<RobotArmCUI>,
+                             RTC::Delete<RobotArmCUI>);
+  }
+  
+};
+
+
---
+++ RobotArmCUI/test/src/RobotArmCUITest.cpp
@@ -0,0 +1,165 @@
+﻿// -*- C++ -*-
+/*!
+ * @file  RobotArmCUITest.cpp
+ * @brief ModuleDescription
+ * @date $Date$
+ *
+ * $Id$
+ */
+
+#include "RobotArmCUITest.h"
+
+// Module specification
+// <rtc-template block="module_spec">
+static const char* robotarmcui_spec[] =
+  {
+    "implementation_id", "RobotArmCUITest",
+    "type_name",         "RobotArmCUITest",
+    "description",       "ModuleDescription",
+    "version",           "1.0.0",
+    "vendor",            "VenderName",
+    "category",          "Category",
+    "activity_type",     "PERIODIC",
+    "kind",              "DataFlowComponent",
+    "max_instance",      "1",
+    "language",          "C++",
+    "lang_type",         "compile",
+    ""
+  };
+// </rtc-template>
+
+/*!
+ * @brief constructor
+ * @param manager Maneger Object
+ */
+RobotArmCUITest::RobotArmCUITest(RTC::Manager* manager)
+    // <rtc-template block="initializer">
+  : RTC::DataFlowComponentBase(manager),
+    m_targetPos_outputOut("targetPos_output", m_targetPos_output)
+
+    // </rtc-template>
+{
+}
+
+/*!
+ * @brief destructor
+ */
+RobotArmCUITest::~RobotArmCUITest()
+{
+}
+
+
+
+RTC::ReturnCode_t RobotArmCUITest::onInitialize()
+{
+  // Registration: InPort/OutPort/Service
+  // <rtc-template block="registration">
+  // Set InPort buffers
+  addInPort("targetPos_output", m_targetPos_outputIn);
+  
+  // Set OutPort buffer
+  
+  // Set service provider to Ports
+  
+  // Set service consumers to Ports
+  
+  // Set CORBA Service Ports
+  
+  // </rtc-template>
+
+  // <rtc-template block="bind_config">
+  // </rtc-template>
+  
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t RobotArmCUITest::onFinalize()
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RobotArmCUITest::onStartup(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RobotArmCUITest::onShutdown(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+RTC::ReturnCode_t RobotArmCUITest::onActivated(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t RobotArmCUITest::onDeactivated(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t RobotArmCUITest::onExecute(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t RobotArmCUITest::onAborting(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RobotArmCUITest::onError(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RobotArmCUITest::onReset(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RobotArmCUITest::onStateUpdate(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t RobotArmCUITest::onRateChanged(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+
+extern "C"
+{
+ 
+  void RobotArmCUITestInit(RTC::Manager* manager)
+  {
+    coil::Properties profile(robotarmcui_spec);
+    manager->registerFactory(profile,
+                             RTC::Create<RobotArmCUITest>,
+                             RTC::Delete<RobotArmCUITest>);
+  }
+  
+};
+
+
