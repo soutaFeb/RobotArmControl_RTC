bbdef2d34954b888caa9e0c9064efa801064ab00
soutaFeb <72483942+soutaFeb@users.noreply.github.com>
2020-11-27 20:17:03 +0900

    update

--- arduino_SerialOutAngleRTC/arduino_SerialOutAngleRTC.ino
+++ arduino_SerialOutAngleRTC/arduino_SerialOutAngleRTC.ino
@@ -1,8 +1,12 @@
 #include <Servo.h> 
 
 Servo servo[3];
-int angle = 0;
+int angle_upper = 0;
+float angle_lower = 0;
+float angle = 0;
 boolean switchDirection[3];
+float upperLimit[3];
+float lowerLimit[3];
 
 void setup() {
   Serial.begin(9600);
@@ -12,18 +16,28 @@ void setup() {
     servo[i].attach(10 + i);
     servo[i].write(90);
   }
+
   switchDirection[0] = false;
   switchDirection[1] = true;
   switchDirection[2] = false;
-  
+
+  upperLimit[0] = 2500;
+  lowerLimit[0] = 500;
+  upperLimit[1] = 2500;
+  lowerLimit[1] = 500;
+  upperLimit[2] = 2500;
+  lowerLimit[2] = 500;
 }
 
 void loop() {
-  if (Serial.available() > 2) {
+  if (Serial.available() > 5) {
     for(int i=0;i<3;i++){
-      angle = Serial.read();
-      if(switchDirection[i])angle = 180 - angle;
-      servo[i].write(angle);
+      angle_upper = Serial.read() * 10;
+      angle_lower = Serial.read() / 10;
+      angle = angle_upper + angle_lower;
+      if(switchDirection[i])angle = 180.0 - angle;
+      angle = map(angle,0,180,lowerLimit[i],upperLimit[i]);
+      servo[i].writeMicroseconds(int(angle));
     }
   }
 }
